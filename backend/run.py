4ff15206756faa8f98b4fcb2d8bb5d3969e44de7
yusuke122 <youfufumu@gmail.com>
2024-08-24 08:12:53 +0900

    ライブラリインストールとバックエンド側run.py更新

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,17 @@
-from app.app import app
+# from app.app import app
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+    
+    
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='../frontend/dist/static', template_folder='../frontend/dist')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
