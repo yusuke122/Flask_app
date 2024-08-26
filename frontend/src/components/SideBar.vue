62fd557b2e263cb06dbb0b8c9cb97ad9daac494f
yusuke122 <youfufumu@gmail.com>
2024-08-26 20:50:57 +0900

    bootstrap対応済み　bootstrap-vueである必要はない

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -1,5 +1,6 @@
 <template>
     <div>sidebar{{title}}</div>
+    <button type="button" class="btn btn-primary">Bootstrap</button>
 </template>
   
   <script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
+import { createApp/*,Vue*/ } from 'vue'
+//import /*Vue,*/ { createApp } from '@vue/compat';
 import App from './App.vue'
+//import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+//import Sidebar from './components/SideBar.vue;
+
+import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+
+//Vue.use(BootstrapVue);
+//Vue.use(IconsPlugin);
+//const app = createApp(App);
+//app.mount('#app');
 createApp(App).mount('#app')
