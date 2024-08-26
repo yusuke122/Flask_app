f5d8fe416b1c005016eb55811e7503493c1052b5
yusuke122 <youfufumu@gmail.com>
2024-08-26 19:28:00 +0900

    SIdeBar.vueの追加

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome!! to Your Vue.js App"/>
+  <SideBar title="だぞ💛"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SideBar from './components/SideBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    SideBar
   }
 }
 </script>
