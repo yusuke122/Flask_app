912a0e19c1034a63c324cf33872ff61f6fcc2b79
yusuke122 <youfufumu@gmail.com>
2024-08-26 20:14:05 +0900

    npm i eslint eslint-plugin-vueでeslintのextentionsのエラー修正

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-  /*
 module.exports = {
   assetsDir: 'static',
-};*/
+  chainWebpack: config => {
+  config.module.rules.delete('eslint');
+}
+};
